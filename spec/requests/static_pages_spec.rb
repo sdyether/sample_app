21cf345638c202ea8c2464c22fc68e3840e08d6a
Shane Dyer <sdye1991@gmail.com>
2014-08-14 19:35:42 +1000

    Refactors title tests.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	describe "Home page" do
 
 		it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
 		it "should have the right title" do
 			visit '/static_pages/home'
-			expect(page).to have_title('Home')
+			expect(page).to have_title("#{base_title} | Home")
 		end
 	end
 
@@ -24,7 +26,7 @@ describe "StaticPages" do
 
 		it "should have the right title" do
 			visit '/static_pages/help'
-			expect(page).to have_title('Help')
+			expect(page).to have_title("#{base_title} | Help")
 		end
 
 	end
@@ -38,7 +40,7 @@ describe "StaticPages" do
 
 		it "should have the right title" do
 			visit '/static_pages/about'
-			expect(page).to have_title('About Us')
+			expect(page).to have_title("#{base_title} | About Us")
 		end	    
 
  	 end
@@ -47,7 +49,7 @@ describe "StaticPages" do
 
  	 	it "should have the right title" do
  	 		visit '/static_pages/contact'
- 	 		expect(page).to have_title('Contact')
+ 	 		expect(page).to have_title("#{base_title} | Contact")
  	 	end
  	 end
 end
